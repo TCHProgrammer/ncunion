22b88cc498becfe9677b48c7287b5abaf4a83238
Anatoly <anatoly54nsk@gmail.com>
2018-10-11 12:54:41 +0700

    templates: add City create

---
+++ backend/views/object_settings/city/create.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\Breadcrumbs;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\object\City */
+
+$this->title = 'Добавить город';
+$this->params['breadcrumbs'][] = [
+    'label' => 'Каталог объектов',
+    'url' => ['/object']
+];
+$this->params['breadcrumbs'][] = ['label' => 'Список городов', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tag-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [], ]); ?>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
