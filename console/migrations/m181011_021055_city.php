1d471b4fb2ae53600e4a20fefa01a10d3fd6a607
Anatoly <anatoly54nsk@gmail.com>
2018-10-11 09:13:49 +0700

    Add migration for city table

---
+++ console/migrations/m181011_021055_city.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181011_021055_city
+ */
+class m181011_021055_city extends Migration
+{
+  private $tableName = 'city';
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+      $this->createTable($this->tableName, [
+        'id' => $this->primaryKey(),
+        'name' => $this->text()->notNull()
+      ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+      $this->dropTable($this->tableName);
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181011_021055_city cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
