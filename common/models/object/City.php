8f9d6ad97a5a08c8782ea61c7675f0f8cc40fe32
Anatoly <anatoly54nsk@gmail.com>
2018-10-11 12:07:57 +0700

    Models: add City entity

---
+++ common/models/object/City.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace common\models\object;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ */
+class City extends ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 50],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Название',
+        ];
+    }
+}
