644c8eb32c972051c145475933b78d0e01e2dc35
Anatoly <anatoly54nsk@gmail.com>
2018-10-12 15:04:00 +0700

    templates: add mkad field into city form

--- backend/views/object_settings/city/_form.php
+++ backend/views/object_settings/city/_form.php
@@ -12,7 +12,8 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+  <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+  <?= $form->field($model, 'mkad')->checkbox() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
