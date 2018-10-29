032fcdf96c21ae4288317e383a8fee49de6ee629
Dizirator <dizirator@gmail.com>
2018-10-29 09:52:41 +0300

    Added update smsc pass

--- common/components/Smsc.php
+++ common/components/Smsc.php
@@ -5,7 +5,7 @@ use Yii;
 use yii\helpers\FileHelper;
 
 define("SMSC_LOGIN", "Natali37");			// логин клиента
-define("SMSC_PASSWORD", "AzsLNWLdqH");	    // пароль или MD5-хеш пароля в нижнем регистре
+define("SMSC_PASSWORD", "iy4QIOdqJWKx");	    // пароль или MD5-хеш пароля в нижнем регистре
 define("SMSC_POST", 0);					    // использовать метод POST
 define("SMSC_HTTPS", 0);				    // использовать HTTPS протокол
 //define("SMSC_CHARSET", "windows-1251");	// кодировка сообщения: utf-8, koi8-r или windows-1251 (по умолчанию)
