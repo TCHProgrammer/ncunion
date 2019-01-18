19da11d4996df279b4dca9d0954fd6b70aa0c1ff
Sergey Kashitsyn <misha.rhapsody@gmail.com>
2019-01-18 12:43:51 +0300

    Style fix (activation page)

--- frontend/views/check-user/email.php
+++ frontend/views/check-user/email.php
@@ -3,6 +3,8 @@ use yii\helpers\Url;
 $this->title = 'Подтверждение электронной почты';
 ?>
 
+<h1><?= $this->title; ?></h1>
+
 <?= $text ?>
 
 <a href="<?= Url::toRoute('/user/profile'); ?>">Для дальнейшер работы перейдите в свой профиль</a>
