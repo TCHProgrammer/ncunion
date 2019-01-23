9632141ef0500187d700f04101e98616dd10e4b7
Anatoly <anatoly54nsk@gmail.com>
2019-01-23 23:41:14 +0700

    add edit Object button

--- common/widgets/tools_panel/views/_tools-panel.php
+++ common/widgets/tools_panel/views/_tools-panel.php
@@ -14,4 +14,7 @@ if (isset($asset)) {
     <?php if (Yii::$app->user->can('can_create_object')): ?>
         <?= Html::a('Создать объект', Url::to(Yii::$app->urlManagerBackend->createAbsoluteUrl('object/create'), true), ['class' => 'btn btn-success']) ?>
     <?php endif; ?>
+    <?php if (Yii::$app->controller->id === 'catalog' && Yii::$app->controller->action->id === 'view' && Yii::$app->user->can('can_edit_object')): ?>
+        <?= Html::a('Редактировать объект', Url::to(Yii::$app->urlManagerBackend->createAbsoluteUrl('object/view'), true).'?id='.Yii::$app->request->get('id'), ['class' => 'btn btn-warning']) ?>
+    <?php endif; ?>
 </div>
