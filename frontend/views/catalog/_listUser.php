9f5365de2b41105b5bfbe011427a57df63911550
Anatoly <anatoly54nsk@gmail.com>
2019-02-04 21:53:07 +0700

    add can_view_investor_info rule usage

--- frontend/views/catalog/_listUser.php
+++ frontend/views/catalog/_listUser.php
@@ -9,20 +9,30 @@ use yii\helpers\Html;
             <div class="profile-image catalog-user-avatar float-md-right"> <img src="<?= isset($model['userAvatar']->avatar) ? $model['userAvatar']->avatar : '/img/other/default-avatar.png' ?>"> </div>
         </div>
         <div class="col-lg-6 col-md-8 col-12">
-            <h4 class="m-t-5 m-b-0">
-                ФИО:
-                <a href="<?= Url::toRoute('/admin/users/view?id=' ) . $model->user_id ?>" target="_blank">
-                    <?= $model['user']->last_name . ' ' . $model['user']->first_name . ' ' . $model['user']->middle_name ?>
-                </a>
-            </h4>
-            <p>Cумма: <?= $model->sum ?></p>
-            <p>Ставка: <?= $model->rate ?></p>
-            <p>Срок: <?= $model->term ?></p>
-            <p>График платежей: <?= ($model->schedule_payments === 1)?'шаровый':'аннуитетный'; ?></p>
-            <p>НКС: <?= $model->nks ?></p>
-            <?php if($model->comment){ ?>
-                <p>Пожелание: <?= $model->comment ?></p>
-            <?php } ?>
+            <?php if ($model->user_id === Yii::$app->user->id  || Yii::$app->user->can('can_view_investor_info')): ?>
+                <h4 class="m-t-5 m-b-0">
+                    ФИО:
+                    <a href="<?= Url::toRoute('/admin/users/view?id=' ) . $model->user_id ?>" target="_blank">
+                        <?= $model['user']->last_name . ' ' . $model['user']->first_name . ' ' . $model['user']->middle_name ?>
+                    </a>
+                </h4>
+                <p>Cумма: <?= $model->sum ?></p>
+                <p>Ставка: <?= $model->rate ?></p>
+                <p>Срок: <?= $model->term ?></p>
+                <p>График платежей: <?= ($model->schedule_payments === 1)?'шаровый':'аннуитетный'; ?></p>
+                <p>НКС: <?= $model->nks ?></p>
+                <?php if($model->comment){ ?>
+                    <p>Пожелание: <?= $model->comment ?></p>
+                <?php } ?>
+            <?php else: ?>
+                <h4 class="m-t-5 m-b-0">
+                    ИД: <?= $model->user_id ?>
+                </h4>
+                <p>Cумма: <?= $model->sum ?></p>
+                <p>Ставка: <?= $model->rate ?>  </p>
+                <p>Срок: <?= $model->term ?></p>
+                <p>График платежей: <?= ($model->schedule_payments === 1)?'шаровый':'аннуитетный'; ?></p>
+            <?php endif; ?>
         </div>
     </div>
 </div>
